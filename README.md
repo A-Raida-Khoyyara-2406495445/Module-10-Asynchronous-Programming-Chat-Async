d0dd7a6d9d1cda3c97f130a0c71d1516f0f94e78
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-05-18 12:50:25 +0700

    Experiment 2.1: Original code, and how it run

---
+++ README.md
@@ -0,0 +1,5 @@
+# Experiment 2.1: Original code, and how it run
+
+## Screenshot
+
+![Experiment 2.1](assets/images/experiment21.png)
---
+++ src/client.rs
@@ -0,0 +1,40 @@
+use futures_util::sink::SinkExt;
+use futures_util::stream::StreamExt;
+use std::error::Error;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let (mut ws_stream, _) =
+        ClientBuilder::from_uri("ws://127.0.0.1:2000".parse().unwrap())
+            .connect()
+            .await?;
+
+    let stdin = io::stdin();
+    let mut stdin_lines = BufReader::new(stdin).lines();
+
+    loop {
+        tokio::select! {
+            incoming = ws_stream.next() => {
+                match incoming {
+                    Some(Ok(msg)) => {
+                        if let Some(text) = msg.as_text() {
+                            println!("From server: {}", text);
+                        }
+                    }
+                    _ => break,
+                }
+            }
+            line = stdin_lines.next_line() => {
+                match line {
+                    Ok(Some(text)) => {
+                        ws_stream.send(Message::text(text)).await?;
+                    }
+                    _ => break,
+                }
+            }
+        }
+    }
+    Ok(())
+}
